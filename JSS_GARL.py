1f5a551120fcd937c9c8d8ae22800f815e79f234
Joel-dev10 <joelthomas7721@gmail.com>
2024-10-29 17:15:20 +0530

    Update during Initialization

--- JSS_GARL.py
+++ JSS_GARL.py
@@ -1,3 +1,6 @@
+import random
+from collections import deque
+
 """Minimal Job Shop Problem"""
 def main() -> None:
     jobs_data = [  # task = (machine_id, processing_time).
@@ -7,7 +10,12 @@ def main() -> None:
     ]
 
     machine_count = 1 + max(task[0] for job in jobs_data for task in job)
-    all_machines = range(machine_count)
+    job_count = len(jobs_data)
+    job_queues = [deque(job) for job in jobs_data]
+    while any(queue for queue in job_queues):   # Continue while any queue is not empty.
+        job = random.randint(0,job_count-1)
+        if job_queues[job]: #Check if the specific queue is not empty.
+            task = job_queues[job].popleft()
 
 
 if __name__ == "__main__":
