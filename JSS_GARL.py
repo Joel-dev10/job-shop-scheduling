923376505e69eae20941adb18f803d1266e496b1
Joel-dev10 <joelthomas7721@gmail.com>
2024-10-29 18:02:57 +0530

    Update to initialize part

--- JSS_GARL.py
+++ JSS_GARL.py
@@ -12,10 +12,14 @@ def main() -> None:
     machine_count = 1 + max(task[0] for job in jobs_data for task in job)
     job_count = len(jobs_data)
     job_queues = [deque(job) for job in jobs_data]
+    job_endtime = [0 for _ in jobs_data]    # Will give the possible start time.
+    schedule = [deque() for _ in range(machine_count)]
+    print(schedule)
     while any(queue for queue in job_queues):   # Continue while any queue is not empty.
         job = random.randint(0,job_count-1)
-        if job_queues[job]: #Check if the specific queue is not empty.
+        if job_queues[job]: # Check if the specific queue is not empty.
             task = job_queues[job].popleft()
+            start_time = job_endtime[job]
 
 
 if __name__ == "__main__":
