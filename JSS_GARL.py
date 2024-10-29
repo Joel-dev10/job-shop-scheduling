32d811cc3212199f7d0fa87067f7ffde28723958
Joel-dev10 <joelthomas7721@gmail.com>
2024-10-29 13:18:14 +0530

    Create JSS_GARL.py

---
+++ JSS_GARL.py
@@ -0,0 +1,9 @@
+"""Minimal Job Shop Problem"""
+jobs_data = [  # task = (machine_id, processing_time).
+        [(0, 3), (1, 2), (2, 2)],  # Job0
+        [(0, 2), (2, 1), (1, 4)],  # Job1
+        [(1, 4), (2, 3)],  # Job2
+    ]
+
+machine_count = 1 + max(task[0] for job in jobs_data for task in job)
+all_machines = range(machine_count)
