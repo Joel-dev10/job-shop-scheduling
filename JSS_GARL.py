e69da913249a70a9e840763b7c8406709943d04f
Joel-dev10 <joelthomas7721@gmail.com>
2024-10-30 15:35:27 +0530

    Update JSS_GARL.py

--- JSS_GARL.py
+++ JSS_GARL.py
@@ -45,7 +45,7 @@ def main() -> None:
     jobs_data = [  # task = (machine_id, processing_time).
         [(0, 3), (1, 2), (2, 2)],  # Job0
         [(0, 2), (2, 1), (1, 4)],  # Job1
-        [(1, 4), (2, 3)],  # Job2
+        [(1, 4), (2, 3)]  # Job2
     ]
 
     schedule = create_schedule(jobs_data)  
