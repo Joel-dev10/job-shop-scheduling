79f5598da3a338cf4c61a091a7ff4f5cda3b9178
Joel-dev10 <joelthomas7721@gmail.com>
2024-11-06 11:15:31 +0530

    Update JSS_GARL.py

--- JSS_GARL.py
+++ JSS_GARL.py
@@ -99,10 +99,11 @@ def display(schedule, makespan):
 def main() -> None:
 
     jobs_data = [  # task = (machine_id, processing_time).
-        [(0, 3), (1, 2), (2, 2), (3, 4)],   # Job0
-        [(0, 2), (2, 1), (1, 4)],   # Job1
-        [(1, 4), (2, 3)],   # Job2
-        [(2, 2), (0, 1), (3, 3), (1, 2)]    # Job3
+        [(0, 3), (1, 2), (2, 2), (3, 4)],  # Job0
+        [(0, 2), (2, 1), (1, 4)],  # Job1
+        [(1, 4), (2, 3)],  # Job2
+        [(2, 2), (0, 1), (3, 3), (1, 2)],    # Job3
+        [(3, 4), (1, 2), (3, 1)]   # Job4
     ]
 
     machine_count = 1 + max(task[0] for job in jobs_data for task in job)
