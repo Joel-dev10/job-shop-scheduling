d6631550ce7eec358f1e1982ddb13920a342533e
Joel-dev10 <joelthomas7721@gmail.com>
2024-11-06 11:51:37 +0530

    Update JSS_GARL.py

--- JSS_GARL.py
+++ JSS_GARL.py
@@ -32,6 +32,7 @@ def fitness_function(schedule):
 
 def crossover(parent1, parent2, machine_count):
     crosspoint = random.randint(0, machine_count-1)
+    # Random part of parent1 is crossed over with random part of parent2 - Single Crossover.
     child1 = parent1[:crosspoint] + parent2[crosspoint:]
     child2 = parent2[crosspoint:] + parent1[:crosspoint]
     return child1, child2
@@ -45,6 +46,7 @@ def mutate(child, machine_count):
 
 
 def add(temp_sch, job_start, machine_start, jobs_data, job, task):
+    # If the previous task of the job is completed or if its the first task of the job.
     if task == 0 or (job, task-1) in [col[0] for row in temp_sch for col in row]:
         machine = jobs_data[job][task][0]
         duration = jobs_data[job][task][1]
@@ -53,6 +55,7 @@ def add(temp_sch, job_start, machine_start, jobs_data, job, task):
         machine_start[machine] = start + duration
         job_start[job] = start + duration
     else:
+        # Call the recursive function again and then update the exiting task.
         add(temp_sch, job_start, machine_start, jobs_data, job, task-1)
         machine = jobs_data[job][task][0]
         duration = jobs_data[job][task][1]
@@ -68,10 +71,12 @@ def correction(schedule, jobs_data):
     machine_start = [0 for _ in schedule]
     temp_sch = [deque() for _ in schedule]
     for sch in schedule:
-        for op in sch:             
-            while op[0] not in [col[0] for row in temp_sch for col in row]:
+        for op in sch: 
+            # Check if it is already in the corrected schedule.            
+            while op[0] not in [col[0] for row in temp_sch for col in row]: 
                 job = op[0][0]
                 task = op[0][1]
+                # Calling a recursive function, so as to follow the proper order for validation.
                 add(temp_sch, job_start, machine_start, jobs_data, job, task)
     return temp_sch
 
@@ -110,34 +115,51 @@ def main() -> None:
     population_size = 10
     generation_size = 50
 
+    # Create a population of random schedules and check their fitness.
     population = [create_schedule(jobs_data) for _ in range (population_size)]
     fitness_scores = [fitness_function(schedule) for schedule in population]
 
     for generation in range(generation_size):
         next_population = []
         length = population_size - int(population_size*10/generation_size) - int(generation/population_size)
+        # Sort the population according to the fitness scores and then select the first n schedules. 
+        # The value of n depends on the generation.
         temp_pop = [x for x, _ in sorted(zip(population, fitness_scores), key = lambda pair: pair[1], reverse = True)[:length]]
         next_population.append(temp_pop[0])
         next_population.append(temp_pop[1])
 
+        # Fill the next population until its size is same as the population size.
         while len(next_population) < population_size:
+
+            # Select the parents from the temporary population.
             parent1 = random.choice(temp_pop)
             parent2 = random.choice(temp_pop)
+
+            # Generate the child schedules by crossing over the parents.
             child1, child2 = crossover(parent1, parent2, machine_count)
+
+            # Mutate the children with a low probability.
             if random.random() < 0.1:
                 mutate(child1, machine_count)
             if random.random() < 0.1:
                 mutate(child2, machine_count)
+
+            # Correct the child schedules after the mixup casued in crossover and mutation.
             child1 = correction(child1, jobs_data)
             child2 = correction(child2, jobs_data)
+
+            # Add to the new population.
             next_population.append(child1)
             next_population.append(child2)
 
+        # Make the next population as the current population.
         population = next_population
         fitness_scores = [fitness_function(schedule) for schedule in population]
 
+    # Get the index of the schedule with the highest fitness score.
     idx = fitness_scores.index(max(fitness_scores))
-    schedule = population[idx]
+    schedule = population[idx]  # Find the schedule that has the highest fitness score with the index.
+    # Sort the schedule display according to the start time of each task for each machine.
     for i in range(len(schedule)):
         schedule[i] = sorted(schedule[i], key=lambda op: op[1][0])
     makespan = max(sch[-1][1][1] for sch in schedule)
